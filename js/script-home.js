9a464ec1b6edfd1e8b8f60a16b8f25d178515976
Rômulo Assunção Corrêa <romulo.assuncao.ra@gmail.com>
2023-05-12 00:32:05 -0400

    Finalizado primeira parte
    Finalizado a primeira parte onde foi criado o HTML-CSS-JS e uma breve introdução de API. Falta corrigir alguns bugs e finalizar algumas funcionalidades.

--- js/script-home.js
+++ js/script-home.js
@@ -1,25 +1,29 @@
 function consultaEndereco() {
     let cep = document.querySelector("#cep").value;
-    if(cep.length <= 8) {
-      alert("CEP INVÁLIDO, o CEP deve conter 8 números");
-      return;
-    }
+      if(cep.length != 8){
+        alert("CEP INVÁLIDO, o CEP deve conter 8 números");
+        return;
+      }
   
     let url = `https://viacep.com.br/ws/${cep}/json/`;
-    fetch(url).then(function (response) {
-      
-      response.json().then(function (data) {
+    fetch(url).then(function(response){
+      //console.log(response);
+      response.json().then(function(data){
+        //console.log(data);
         mostrarEndereco(data);
       });
     });
   }
   
-  function mostrarEndereco(dados) {
-    let nomeLocalizacao = document.querySelector(".nome-localizacao");
-    if (dados.erro) {
-      nomeLocalizacao.innerHTML = "CEP não encontrado";
-    } else {
-      nomeLocalizacao.innerHTML = dados.localidade;
-      console.log("aaaa");
-    }
-  }
+
+    function mostrarEndereco(dados){
+      //console.log(dados)
+      let nomeLocalizacao = document.querySelector(".nome-localizacao");
+      if (dados.erro){
+        nomeLocalizacao.innerHTML = "CEP não encontrado";
+        } 
+        else{
+          //console.log(dados.localidade);
+          nomeLocalizacao.innerHTML = dados.localidade;
+        }
+      }
